9668142616efad469cf7e493aa67f537fc436c6e
Obnunnv <obsinungk@gmail.com>
2026-04-13 03:10:25 +0000

    Update analyzer.py

--- scan/analyzer.py
+++ scan/analyzer.py
@@ -1,1 +1,31 @@
+from knowledge_base import SERVICE_INFO
+
+def analyze_results(ports):
+	results = []
+
+	for port, service in ports:
+		service = service.lower()
+
+		info = SERVICE_INFO.get(service)
+
+		if info:
+			results.append({
+				"port": port,
+				"service": service,
+				"risk": info["risk"],
+				"description": info["description"],
+				"danger": info["danger"],
+				"fix": info["fix"]
+			})
+		else:
+			results.append({
+				"port": port,
+				"service": service,
+				"risk": "UNKNOWN",
+				"description": "No known data for this service.",
+				"danger": "Risk cannot be determined.",
+				"fix": "Manually investigate or disable service if unnecessary."
+			})
+
+	return results
 
